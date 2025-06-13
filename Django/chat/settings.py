2d9aa8b234b61fd9e4f6687ccfd6a865859f5b20
IrgenSlj <i.salianji@gmail.com>
2025-06-13 15:27:36 +0200

    General restructure, MoE arch

---
+++ Django/chat/settings.py
@@ -0,0 +1,21 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'chat.apps.ChatConfig',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+    },
+]
+
+OLLAMA_PORT = 11435  # this was 11434
+OLLAMA_HOST = 'localhost'
+OLLAMA_URL = f'http://{OLLAMA_HOST}:{OLLAMA_PORT}/api/chat'
