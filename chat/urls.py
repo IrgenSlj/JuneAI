8ef520c254132115c06536ae96f3859fce356860
IrgenSlj <i.salianji@gmail.com>
2025-06-13 16:05:02 +0200

    Update .gitignore, remove old files, and add training directory

--- chat/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chat_view, name='chat'),
-    path('chat/send/', views.send_message, name='send_message'),
-]
