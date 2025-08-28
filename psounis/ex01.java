d190b0d5d7c9c3bd71f9898b7750f6bd79c6ecab
IrgenSlj <i.salianji@gmail.com>
2025-08-28 21:49:46 +0200

    Update

---
+++ psounis/ex01.java
@@ -0,0 +1,14 @@
+class ex01 {
+    public static void main(String[] args) {
+        boolean b;
+        b = true;
+        char c = '\u4ADD';
+        System.out.println(b);
+        System.out.println(c);
+        c++;
+        System.out.println(c);
+
+        c++;
+        System.out.println(c);
+    }
+}
