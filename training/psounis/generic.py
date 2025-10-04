33c8d25ede80884d8fcc166e1780b6ed4396cb1a
IrgenSlj <i.salianji@gmail.com>
2025-10-04 21:06:07 +0200

    Update

---
+++ training/psounis/generic.py
@@ -0,0 +1,30 @@
+class MyIterator:
+    def __iter__(self):
+        self.i = 9
+        return self
+    
+    def __next__(self):
+        self.i += 1
+        if self.i <= 20:
+            return self.i ** 2
+        else:
+            raise StopIteration
+        
+for i in MyIterator():
+    print(i)
+
+print("#"*25)
+
+def MyGenerator():
+    for i in range(10, 21):
+        yield i*i
+
+for i in MyGenerator():
+    print(i)
+
+print("#"*25)
+
+g = (i*i for i in range(10, 21))
+
+for i in g:
+    print(i)
