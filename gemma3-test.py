96a041cbe201c04f2ec924abe5b389c7512f5535
IrgenSlj <i.salianji@gmail.com>
2025-10-15 10:47:48 +0200

    Update

--- gemma3-test.py
+++
@@ -1,8 +0,0 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
-
-pipe = pipeline("text-generation", model="google/gemma-3-1b-it") #google/gemma-3-270m")
-messages = [
-    {"role": "user", "content": "Who is the current prime minister of Albania?"},
-]
-pipe(messages)
